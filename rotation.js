6a43ce3d5c7c5b79908f4544b257a436e113073a
nerearodriguezmartin <nerearodrimar2001@gmail.com>
2020-11-08 13:42:31 +0100

    ejercicio

--- rotation.js
+++ rotation.js
@@ -1,12 +1,14 @@
 function inicio(){
 
     if (window.DeviceMotionEvent) {
-        window.addEventListener('devicemotion', orientacion);
+        window.addEventListener("deviceorientation", orientacion);
 
       }
   }
   window.onload = inicio;
 
   function orientacion(e){
-    document.getElementById("log").style.transform = "rotate("+ e.alpha +")";
+    document.getElementById("log").style.transform = "rotateZ(" + ( e.alpha - 180 ) + "deg) " +
+    "rotateX(" + e.beta + "deg) " +
+    "rotateY(" + ( -e.gamma ) + "deg)";
   }
